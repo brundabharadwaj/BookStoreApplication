07087df900dafde9526823de95bf9b8ef30a389b
Brunda M Bharadwaj <brundabharadwaj22@gmail.com>
2025-01-19 16:59:01 +0530

    Adding Basic Testcase for the Controller

---
+++ src/test/java/com/fullstack/workflow/bookstore/api/BookStoreControllerTest.java
@@ -0,0 +1,33 @@
+package com.fullstack.workflow.bookstore.api;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(BookController.class)
+class BookControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void getAllBooks_ReturnsListOfBooks() throws Exception {
+        mockMvc.perform(get("/api/books")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].title").value("You are Awesome"))
+                .andExpect(jsonPath("$[0].author").value("Brunda Bharadwaj"));
+    }
+}
